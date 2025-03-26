186726cd1a6bdf1e9fd22d4f9b04f8fd16980c19
Garrison <garrison_liu@outlook.com>
2025-03-26 15:49:27 +0800

    update docstring

--- src/github_api.py
+++ src/github_api.py
@@ -1,11 +1,11 @@
 import requests
 
 def get_github_repos(username: str) -> list[dict[str, str]]:
-    '''
+    """
     获取 GitHub 用户的公共仓库信息
     :param username: username of GitHub
     :return: A json file containing a list of dict
-    '''
+    """
     url = f"https://api.github.com/users/{username}/repos?type=all&per_page=100"
     response = requests.get(url)
 
@@ -17,12 +17,12 @@ def get_github_repos(username: str) -> list[dict[str, str]]:
 
 
 def get_repo_languages(username: str, repo_name: str) -> dict[str, int]:
-    '''
+    """
     获取仓库的语言使用情况
     :param username: username of GitHub
     :param repo_name: repository name of a repo
     :return: A json file which is a dict of different languages usage
-    '''
+    """
     url = f"https://api.github.com/repos/{username}/{repo_name}/languages"
     response = requests.get(url)
 
--- src/line_counter.py
+++ src/line_counter.py
@@ -1,11 +1,11 @@
 from src.github_api import *
 
 def get_github_lines(username: str) -> int:
-    '''
+    """
     计算 GitHub 用户的总代码行数
     :param username: username of GitHub
     :return: total code lines of a user
-    '''
+    """
     repos = get_github_repos(username)
     total_lines = 0
 
