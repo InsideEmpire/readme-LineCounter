97c7724e422a2de90c117e0c4f76bbd46b889944
Garrison <garrison_liu@outlook.com>
2025-03-28 16:43:29 +0800

    refactor: refactor code with async for better performance

--- src/line_counter.py
+++ src/line_counter.py
@@ -1,22 +1,21 @@
 from src.github_api import *
+import aiohttp
+import asyncio
 
-def get_github_lines(username: str) -> int:
+async def get_github_lines(username: str) -> int:
     """
     计算 GitHub 用户的总代码行数
     :param username: username of GitHub
     :return: total code lines of a user
     """
-    repos = get_github_repos(username)
-    total_lines = 0
+    async with aiohttp.ClientSession() as session:
+        repos = await get_github_repos(session, username)
+        total_lines = 0
 
-    for repo in repos:
-        repo_name = repo['name']
-        print(f"Getting languages for repository: {repo_name}")
+        tasks = [get_repo_languages(session, username, repo['name']) for repo in repos]
+        languages_list = await asyncio.gather(*tasks)
 
-        # 获取该仓库的语言使用情况
-        languages = get_repo_languages(username, repo_name)
+        for languages in languages_list:
+            total_lines += sum(languages.values())
 
-        # 累加所有语言的行数
-        total_lines += sum(languages.values())
-
-    return total_lines
+        return total_lines
